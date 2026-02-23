ea65b951c46612061f498851b76d6dcf6e8b9294
Adithya <adithynarayanan22@gmail.com>
2026-02-23 20:58:38 +0530

    Supperuser,session login and logout

--- AdminApp/urls.py
+++ AdminApp/urls.py
@@ -4,4 +4,7 @@ from AdminApp import views
 urlpatterns=[
     path('dashboard/',views.dashboard,name='dashboard'),
     path('add_student/',views.add_student,name='add_student'),
+    path('login/',views.login,name='login'),
+    path('admin_login/',views.admin_login,name='admin_login'),
+    path('admin_logout/',views.admin_logout,name='admin_logout')
 ]
--- AdminApp/views.py
+++ AdminApp/views.py
@@ -1,8 +1,38 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+
+
 
 # Create your views here.
 def dashboard(request):
     return render(request,'Dashboard.html')
 
+def login(request):
+    return render(request,'Login.html')
+
 def add_student(request):
     return render(request,'Add_Student.html')
+def admin_logout(request):
+    return render(request,'Login.html')
+
+def admin_login(request):
+    uname=request.POST.get('username')
+    pswd=request.POST.get('password')
+    if User.objects.filter(username__contains=uname).exists():
+        user=authenticate(username=uname,password=pswd)
+        if user is not None:
+            # login(request,user)
+            request.session['username']=uname
+            request.session['password']=pswd
+            return redirect(dashboard)
+        else:
+            return redirect(login)
+    else:
+        return redirect(login)
+def admin_logout(request):
+    del request.session['username']
+    del request.session['password'] 
+    return redirect(login)
+
